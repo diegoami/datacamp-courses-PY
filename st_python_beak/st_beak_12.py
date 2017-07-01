1686764f8924c14fea4b96ef721d9a63c21ea156
diegoami <diego.amicabile@gmail.com>
2017-07-01 22:09:56 +0200

    Finished courses of datacamp

---
+++ st_python_beak/st_beak_12.py
@@ -0,0 +1,29 @@
+from datacamp.lib import *
+from data.beaks_parent_offspring import *
+
+
+def heritability(parents, offspring):
+    """Compute the heritability from parent and offspring samples."""
+    covariance_matrix = np.cov(parents, offspring)
+    return covariance_matrix[0, 1] / covariance_matrix[0, 0]
+
+
+# Compute the heritability
+heritability_scandens = heritability(bd_parent_scandens, bd_offspring_scandens)
+heritability_fortis = heritability(bd_parent_fortis, bd_offspring_fortis)
+
+# Initialize array of replicates: perm_replicates
+perm_replicates = np.empty(10000)
+
+# Draw replicates
+for i in range(10000):
+    # Permute parent beak depths
+    bd_parent_permuted = np.random.permutation(bd_parent_scandens)
+    perm_replicates[i] = heritability(bd_parent_permuted, bd_offspring_scandens)
+
+
+# Compute p-value: p
+p = np.sum(perm_replicates >= heritability_scandens) / len(perm_replicates)
+
+# Print the p-value
+print('p-val =', p)
