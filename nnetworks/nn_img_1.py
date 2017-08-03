75363eb1328e54e9d38f0d98f405f1975c9d4725
diegoami <diego.amicabile@gmail.com>
2017-08-03 12:51:13 +0200

    Completed Deep learning course.

---
+++ nnetworks/nn_img_1.py
@@ -0,0 +1,26 @@
+import keras
+import matplotlib.pyplot as plt
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.utils import to_categorical
+import pandas as pd
+from keras.callbacks import EarlyStopping
+
+# Create the model: model
+model = Sequential()
+
+# Add the first hidden layer
+model.add(Dense(50, activation='relu', input_shape = (784,)))
+
+# Add the second hidden layer
+model.add(Dense(50, activation='relu'))
+
+# Add the output layer
+model.add(Dense(10, activation='softmax'))
+
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Fit the model
+model.fit(X, y, validation_split=0.3)
